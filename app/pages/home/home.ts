997f987a51a1e2c6605263ec936e9a1b0c19722d
Satit Rianpit <rianpit@gmail.com>
2016-09-10 18:55:52 +0700

    success

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -6,7 +6,14 @@ import {NavController} from 'ionic-angular';
 })
 export class HomePage {
 
+  employees: any[]
+
   constructor(private navCtrl: NavController) {
+    this.employees = [
+      {id: 1001, name: 'Steve Job'},
+      {id: 1002, name: 'John Doe'},
+      {id: 1003, name: 'Bill Gate'},
+    ]
 
   }
 
