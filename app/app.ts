d16fa9a7a099ff3d8c8c599e716f293165d2d299
Satit Rianpit <rianpit@gmail.com>
2016-09-11 14:43:23 +0700

    not success

--- app/app.ts
+++ app/app.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
 import {Platform, ionicBootstrap} from 'ionic-angular';
-import {StatusBar} from 'ionic-native';
+import {StatusBar, Dialogs} from 'ionic-native';
 import {TabsPage} from './pages/tabs/tabs';
 
 import {LoginPage} from './pages/login/login'
@@ -22,6 +22,16 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
     });
+
+    platform.registerBackButtonAction(() => {
+      Dialogs.confirm('คุณต้องการออกจากระบบ?', 'ยืนยัน', ['ยกเลิก', 'ยืนยัน'])
+        .then(btnIndex => {
+          if (btnIndex == 2) {
+            platform.exitApp();
+          }
+        });
+    });
+
   }
 }
 
--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,14 +1,20 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
 
+import {LoginPage} from '../login/login'
+
 @Component({
   templateUrl: 'build/pages/home/home.html'
 })
   
 export class HomePage {
-
+  
   constructor(private navCtrl: NavController) {
   
+  }
+
+  logout() {
+    this.navCtrl.setRoot(LoginPage)
   }
 
 }
