c2caec96c48dfeca772ebb67f874f84e9b754ca5
Satit Rianpit <rianpit@gmail.com>
2016-09-11 14:01:41 +0700

    success select

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -7,8 +7,38 @@ import {NavController} from 'ionic-angular';
   
 export class HomePage {
 
+  mainDepartments: any[]
+  subDepartments: any[]
+  allSubDepartments: any[]
+
+  mainDep: number
+  subDep: number
+
   constructor(private navCtrl: NavController) {
-  
+    
+    this.mainDepartments = [
+      { id: 1, name: 'การพยาบาล' },
+      { id: 2, name: 'ฝ่ายส่งเสริม' },
+      { id: 3, name: 'ศูนย์ประกัน' }
+    ]
+    
+    this.allSubDepartments = [
+      { id: 1, mainDep: 1, name: 'แผนก ER' },
+      { id: 2, mainDep: 2, name: 'ฝากครรภ์' },
+      { id: 3, mainDep: 2, name: 'ฉีดวัคซีน' },
+      { id: 4, mainDep: 3, name: 'ศูนย์คอมพิวเตอร์' }
+    ]
+
+  }
+
+  getSubDepartments() {
+    // alert(this.mainDep)
+    this.subDepartments = []
+
+    this.allSubDepartments.forEach(v => {
+      if (v.mainDep == this.mainDep) this.subDepartments.push(v)
+    })
+
   }
 
 }
